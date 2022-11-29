d29847955a82737537dd51055c8cc2d29056c607
Rajneesh Verma <rjnshvrm23@gmail.com>
2022-11-29 23:04:40 +0530

    trying to get list of links and prices

--- app/soupy/search.py
+++ app/soupy/search.py
@@ -1,7 +1,17 @@
+import os
 import requests
 from bs4 import BeautifulSoup
+from dotenv import load_dotenv
 
-response = requests.get("https://news.ycombinator.com/")
+load_dotenv()
+
+header = {
+    "User-Agent": "my user agent info",
+    "Accept-Language": "en-US,en;q=0.9"
+}
+
+response = requests.get("https://www.zillow.com/oxford-ms/apartments/1-bedrooms/", headers=header)
+response.raise_for_status()
 soup = BeautifulSoup(response.text, "html.parser")
-news = soup.find_all(name="span", class_="titleline")
-print(news)
+data = soup.find_all(class_="list-card-info")
+
